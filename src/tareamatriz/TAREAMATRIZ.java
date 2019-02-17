5d4372cff590259ca14004614eea6b900626d6f9
kloarc98 <kevinoloarca@gmail.com>
2019-02-16 19:57:56 -0600

    funciones

--- src/tareamatriz/TAREAMATRIZ.java
+++ src/tareamatriz/TAREAMATRIZ.java
@@ -11,14 +11,9 @@ import javax.swing.JOptionPane;
  * Kevin Oswaldo Loarca Fuentes
  */
 public class TAREAMATRIZ {
-
-    /**
-     * @param args the command line arguments
-     */
-        public static void main(String[] args) {
-            
-            //Declara el scanner
-		Scanner entrada = new Scanner (System.in);
+    
+    public static void Matriz(){
+        
 		//Declara las variables para departamento
 		int Departamento[];
 		Departamento = new int[5];
@@ -27,16 +22,11 @@ public class TAREAMATRIZ {
 		Matriz=new String[10][7];
 		
                 //Declaramos todas las varibles necesarias
-		String descuentos,bonificaciones,sueldoLiquido,departamento,sueldoBase,isrver;
-		int opcion, des,boni,suelLiq=0,dep=0,sB,total;
-		int isr1= (int) 0.03;
-		int isr2= (int) 0.05;
-		int isr3= (int) 0.10;
+		String descuentos,bonificaciones,sueldoLiquido,departamento,sueldoBase,isrver = null;
+		int des,boni,suelLiq=0,dep=0,sB,total;
                 float resp = 0;
                 
-                //ingresos hacia la matriz
-		do {
-			for(int i=0;i<10;i++) {
+                for(int i=0;i<10;i++) {
 				//Ingreso de nombre
                                 Matriz[i][0]=JOptionPane.showInputDialog(null,"Ingrese el nombre:"+(i+1));
 				//Se utiliza ramdom para que salga el sueldo base
@@ -53,20 +43,16 @@ public class TAREAMATRIZ {
 				Matriz[i][3]=bonificaciones;
 				//Se usa un if para saber que porcentaje de isr usara 
                                 if(sB>2600 && sB<5000){
-                                    resp = (float) isr1 * sB;
-                                    isrver = Integer.toString((int) (float) resp);
-                                    Matriz[i][4]=isrver;
+                                    resp=(float) ((sB) * 0.03);
+				Matriz[i][4]=String.valueOf(resp);
                                 }else if(sB>5001 && sB<10000){
-                                    resp = (float) isr1 * sB;
-                                    isrver = Integer.toString((int) (float) resp);
+                                    resp=(float) ((sB) * 0.05);
+				Matriz[i][4]=String.valueOf(resp);
                                 Matriz[i][4]=isrver;
                                 }else if (sB>10001 && sB<100000){
-                                    resp = (float) isr1 * sB;
-                                    isrver = Integer.toString((int) (float) resp);
-                                    Matriz[i][4]=isrver;
+                                   resp=(float) ((sB) * 0.10);
+				Matriz[i][4]=String.valueOf(resp);
                                 }
-                                isrver = Integer.toString((int) resp);
-                                Matriz[i][4]=isrver;
 				//Se suma el sueldo y la bonificacion menos descuentos e isr
 				suelLiq=(int) (sB-des+boni-resp);
 				sueldoLiquido=Integer.toString(suelLiq);
@@ -77,8 +63,6 @@ public class TAREAMATRIZ {
 				Matriz[i][6]=departamento;
 				
 			}
-                        
-                        
 			
 			System.out.println("Nombres\t  Sueldo Base\tDescuentos\tBonificacion\tISR\tSueldo Liquido\tNo Departamento");
 			for(int j=0;j<10;j++) {
@@ -86,49 +70,47 @@ public class TAREAMATRIZ {
 			
 			}
 			
-			   
-			     
-			     
-			      for(int i = 0; i<10;i++)
-			      { 
+			      for(int i = 0; i<10;i++){ 
 			    	  total = Integer.parseInt(Matriz[i][5]);
-			      if(Matriz[i][6].equals("1"))
-			      {
+			      if(Matriz[i][6].equals("1")){
 			    	  Departamento[0] = Departamento[0] + total;
 			      }
-			      if(Matriz[i][6].equals("2"))
-			      {
+			      if(Matriz[i][6].equals("2")){
 			    	  Departamento[1] = Departamento[1] + total;
 			      }
-			       if(Matriz[i][6].equals("3"))
-			      {
+			       if(Matriz[i][6].equals("3")){
 			    	   Departamento[2] = Departamento[2] + total;
 			      }
-			      if(Matriz[i][6].equals("4"))
-			      {
+			      if(Matriz[i][6].equals("4")){
 			    	  Departamento[3] = Departamento[3] + total;
 			      }
-			      if(Matriz[i][6].equals("5"))
-			      {
+			      if(Matriz[i][6].equals("5")){
 			    	  Departamento[4] = Departamento[4] + total;
 			      }
-			     
 			      }
                               
                               System.out.println("");
-			       for(int i = 0;i<=4;i++)
-			       {
+			       for(int i = 0;i<=4;i++){
 			           System.out.println("Departamento "+(i+1)+": "+Departamento[i]);
-			       }
-			 System.out.println("");
-			System.out.print("Desea Ingresar todo de nuevo?\n 1.si\n 2.Salir\n");
-			opcion=entrada.nextInt();
+			       }		 
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+            
+            //Declara el opcion
+		int opcion=0;
+                Scanner entrada = new Scanner (System.in);
+                //ingresos hacia la matriz
+		do {
+                    Matriz();
+                System.out.println("");
+		System.out.print("Desea Ingresar todo de nuevo?\n 1.si\n 2.Salir\n");
+		opcion=entrada.nextInt();
+			
 		}while(opcion!=2);
-		
-		
-		
 		
 	}
     }
-    
-
